d473d240c2d9e5b7dd31e537b8ab361c805c36c7
Rafaela Franco <rafaelafranco096@gmail.com>
2025-11-23 12:19:57 -0400

    Create first component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+//maioria dos casos se utiliza jsx
+//precisa dele sempre que se cria um component
+//funcao que utiliza o nome em pascal case - PascalCase
+//App
+//HeaderHeading
+//o que retorna da funcao é o que é exibido na pagina
+//se tiver mais de uma linha no retorno, é recomendado que se use parenteses - nesse caso o prettier ja ta fazendo isso
+//pode reutilizar quantas vezes quiser o component
+//jsx só permite que voce retorne UM elemento pai por component
+//a tag fechada e vazia se chama react fragment - é uma tag vazia, que funciona como elemento pai dos demais -> esse tipo de coisa funciona no react native?
+//nesse caso, ao inspecionar, nao fica mais aparecendo uma div la
+//pode ser util para nao quebrar layout
+//poderia importar, por ser um elemento do react, <Fragment>
+export function App() {
+  console.log('Oi');
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <h1>Hello World!</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
       </p>
     </>
   );
 }
 
-export default App;
+// export { App }; // -> uma forma de exportar, mas ao importar, pode se utilizar qualquer nome
+//dessa forma é um export nomeado, entao obrigatoriamente tem que usar chaves e o nome correto ao importar
+//pra facilitar, pode ja exportar a funcao, que dá exatamente na mesma coisa que foi feito aqui
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { App } from './App';
+
 //busca por um elemento da pagina que tem o id root
 //strictmode é basicamente uma boa pratica, pra garantir que enquanto ta desenvolvendo a
 //pagina, a pagina rodaria
@@ -11,6 +11,9 @@ import App from './App.tsx';
 //jsx - uma extensao do html que pode ser usado dentro do js
 //na teoria, pode ter react em apenas um trecho da pagina
 //nao e muito comum, mas pode acontecer
+
+//tem que chamar dentro do strictmode, se nao tem filhos, pode abrir e fechar na mesma tag </>
+//stricMode roda o codigo duas vezes, pra garantir que vai funcionar mesmo em producao
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
