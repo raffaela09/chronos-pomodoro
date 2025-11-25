013488493aacc121c6ea1ecbaa83e273703df7fc
Rafaela Franco <rafaelafranco096@gmail.com>
2025-11-25 08:58:46 -0400

    Create forms.

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,26 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>Task</label>
+            <input id='input' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
